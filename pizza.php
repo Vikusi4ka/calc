a6d157e4df6d484dfdd8e7dda935c8f2e2ec96fc
Vikusi4ka <vikusi4karomanova@rambler.ru>
2013-04-27 16:25:21 +0300

    Create pizza.php
    Перехід на нову сторінку, переглЯд замовлення (php)

---
+++ pizza.php
@@ -0,0 +1,30 @@
+
+
+<?php
+$Pizza_Morskaia=$_REQUEST['Pizza_Morskaia'];
+echo "Pizza_Morskaia=$Pizza_Morskaia <br>";
+$Pizza_Gribu_kovbasa=$_REQUEST['Pizza_Gribu_kovbasa'];
+echo "Pizza_Gribu_kovbasa=$Pizza_Gribu_kovbasa <br>";
+$Pizza_Shunka=$_REQUEST['Pizza_Shunka'];
+echo "Pizza_Shunka=$Pizza_Shunka <br>";
+$Pizza_Chicen=$_REQUEST['Pizza_Chicen'];
+echo "Pizza_Chicen=$Pizza_Chicen <br>";
+$Osnova_pod_pizzy=$_REQUEST['Osnova_pod_pizzy'];
+echo "Osnova_pod_pizzy=$Osnova_pod_pizzy <br>";
+$Pizza_Perec=$_REQUEST['Pizza_Perec'];
+echo "Pizza_Perec=$Pizza_Perec <br>";
+$Pizza_Cur=$_REQUEST['Pizza_Cur'];
+echo "Pizza_Cur=$Pizza_Cur <br>";
+$Pizza_Kykyrydza=$_REQUEST['Pizza_Kykyrydza'];
+echo "Pizza_Kykyrydza=$Pizza_Kykyrydza <br>";
+$Pizza_Ananasu=$_REQUEST['Pizza_Ananasu'];
+echo "Pizza_Ananasu=$Pizza_Ananasu <br>";
+$Pizza_Selen=$_REQUEST['Pizza_Selen'];
+echo "Pizza_Selen=$Pizza_Selen <br>";
+$otrumania=$_REQUEST['otrumania'];
+echo "otrumania=$otrumania <br>";
+$kilkist=$_REQUEST['kilkist'];
+echo "kilkist=$kilkist <br>";
+
+
+?>
